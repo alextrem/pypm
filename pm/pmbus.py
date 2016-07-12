cb5f3f12c2189d7ba2ad60a88945fb39a39d90d9
Alexander Geißler <lancemt@gmx.de>
2016-07-12 23:23:23 +0200

    Added doc strings for functions

--- pm/pmbus.py
+++ pm/pmbus.py
@@ -23,35 +23,150 @@ class Pmbus():
 
     def write_word(self, slave_addr, command, data):
         """
+        Send two bytes over PMBus
+
+        Parameters
+        ----------
+        slave_addr: device address
+        command:
+        data:
+
+        Returns
+        -------
+
         """
 
     def write_byte_pec(self, slave_addr, command, data):
         """
+        Send byte over PMBus
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+        data:
+
+        Returns
+        -------
+
         """
     def write_word_pec(self, slave_addr, command, data):
         """
+        Write word with parity error check
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+        data:
+
+        Returns
+        -------
+
         """
     def send_byte(self, slave_addr, command):
         """
+        Send byte over PMBus
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+
+        Returns
+        -------
+
         """
     def send_byte_pec(self, slave_addr, command):
         """
+        Send byte with parity error check
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+
+        Returns
+        -------
+
         """
     def read_word(self, slave_addr, command):
         """
+        Read two bytes over PMBus
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+
+        Returns
+        -------
+
         """
     def read_word_pec(self, slave_addr, command):
         """
+        Read two bytes over PMBus with parity error check
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+
+        Returns
+        -------
+
         """
     def read_byte(self, slave_addr, command):
         """
+        Read byte over PMBus
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+
+        Returns
+        -------
+
         """
     def read_byte_pec(self, slave_addr, command):
         """
+        Read byte over PMBus with parity error check
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+
+        Returns
+        -------
+
         """
     def read_block(self, slave_addr, command, count):
         """
+        Read a block of data
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+        count: Number of bytes to be read
+
+        Returns
+        -------
+
         """
     def read_block_pec(self, slave_addr, command, count):
         """
+        Read a block of data woth parity error check
+
+        Parameters
+        ----------
+        slave_addr: Device address
+        command:
+        count: Number of bytes to be read
+
+        Returns
+        -------
+
         """
