61cdc1029ae228f1beed2218dff2762be147454a
Alexander Geißler <lancemt@gmx.de>
2017-08-25 07:26:56 +0200

    edited some pylint warnings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -31,12 +31,12 @@ sys.path.insert(0, os.path.abspath('../../'))
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.todo',
-    'sphinx.ext.coverage',
-    'sphinx.ext.imgmath',
-    'sphinx.ext.viewcode',
-    'sphinx.ext.githubpages',
-    'sphinx.ext.napoleon']
+              'sphinx.ext.todo',
+              'sphinx.ext.coverage',
+              'sphinx.ext.imgmath',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.napoleon']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -172,6 +172,3 @@ texinfo_documents = [
      author, 'PyPM', 'One line description of project.',
      'Miscellaneous'),
 ]
-
-
-
