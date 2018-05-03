426ced412253e2d23ecb05f2e4fc495a95a41025
Alexander Geißler <lancemt@gmx.de>
2018-05-03 22:11:40 +0200

    changed indentation

--- setup.py
+++ setup.py
@@ -25,39 +25,39 @@ class PyTest(TestCommand):
         sys.exit(errno)
 
 
-setup(
-        name='pypm',
-        version=versioneer.get_version(),
-        description='Powermanagement command creator',
-        url='http://www.github.com/alextrem/pypm',
-        author='Alexander Geissler',
-        author_email='alextrem@web.de',
-        license='MIT',
-        classifiers=[
-            'Development Status :: 3 - Alpha',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development :: Hardware',
-            'License :: OSI Approved :: MIT License',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 3.3',
-            ],
-        keywords='development powermanagement',
-        cmdclass={
-            '': versioneer.get_cmdclass(),
-            'test': PyTest
-        },
-        packages=find_packages(exclude=['docs', 'tests']),
-        install_requires=['numpy',
-                          'scipy',
-                          'matplotlib'],
-        extras_require={
-            'dev': ['check-manifest'],
-            'test': ['coverage'],
-            },
-        entry_points={
-            'console_scripts': [
-                'pm=pm:main',
-                ],
-            },
-        tests_require=['pytest'],
-        )
+setup(name='pypm',
+      version=versioneer.get_version(),
+      description='Powermanagement command creator',
+      url='http://www.github.com/alextrem/pypm',
+      author='Alexander Geissler',
+      author_email='alextrem@web.de',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Hardware',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.3',
+      ],
+      keywords='development powermanagement',
+      cmdclass={
+          '': versioneer.get_cmdclass(),
+          'test': PyTest
+      },
+      packages=find_packages(exclude=['docs', 'tests']),
+      install_requires=['numpy',
+                        'scipy',
+                        'matplotlib'],
+      extras_require={
+          'dev': ['check-manifest'],
+          'test': ['coverage'],
+      },
+      entry_points={
+          'console_scripts': [
+              'pm=pm:main',
+          ],
+      },
+      tests_require=['pytest',
+                     'sphinx_rtd_theme', ],
+      )
