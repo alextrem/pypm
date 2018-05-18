efbdff880579c434a14ceff45d2fc6c6568d41af
Alexander Geißler <lancemt@gmx.de>
2018-05-18 07:04:03 +0200

    got interface running, some handling still so data is not read twiceneccesary

--- pm/serial_interface.py
+++ pm/serial_interface.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python
 """ This class provides a possibility to read data from a serial interface"""
 
-from numpy import array, zeros
+from numpy import array, zeros, int16
 from threading import Thread
+import collections
 import logging
 import serial
+import struct
 
 # TODO: Add interface as base class
 
@@ -51,7 +53,7 @@ class SerialInterface():
     >>> ser.get_buffer()
 
     """
-    def __init__(self, port='/dev/ttyUSB0', baud='115200', timeout=4):
+    def __init__(self, port='/dev/ttyUSB0', baud='115200', timeout=4.):
         """
         port : str
             port to connect to
@@ -63,33 +65,42 @@ class SerialInterface():
         self._port = port
         self._baud = baud
         self._timeout = timeout
-        self.raw_data = None
+        self.raw_data = bytearray(2)
         self.thread = None
         self.chunk_size = 1024
-        self.buffer = zeros(self.chunk_size)
+        # self.buffer = zeros(self.chunk_size, dtype=int16)
+        self.buffer = collections.deque([0] * self.chunk_size,
+                                        maxlen=self.chunk_size)
         self.log = logging.getLogger(type(self).__name__)
 
         try:
-            self.ser = serial.Serial(port, baud, timeout)
-            self.thread = Thread(target=self.backgroundThread,
+            self.ser = serial.Serial(port, baud, timeout=timeout)
+            self.log.info("Connected to serial port " +self._port)
+            self.thread = Thread(target=self.background_thread,
                                  name="Serial Thread")
             self.thread.start()
         except serial.SerialException:
-            self.log.error("Fehler")
+            self.log.error("Unable to open " +self._port)
 
     def get_buffer(self):
         """
-        Test
+        Provide buffer for further analysis
         """
         self.buffer[:-1]
 
-    def backgroundThread(self):
+    def background_thread(self):
         """
         Collect data in a seperat "background" thread
         """
         self.ser.flushInput()
         while self.ser.isOpen():
             self.ser.readinto(self.raw_data)
+            # Shift buffer one item to the left and assign received data
+            data, = struct.unpack('H', self.raw_data)
+            self.buffer.append(data)
+            # self.buffer[-1] = data
+            # self.buffer[:-1] = self.buffer[1:]
+            print self.buffer
 
     def close(self):
         """
@@ -97,8 +108,12 @@ class SerialInterface():
         """
         self.thread.join()
         self.ser.close()
+        self.log.info("Closing serial connection on" +self._port)
 
     @property
     def chunk_size(self):
         """
         """
+
+if __name__ == '__main__': 
+    ser = SerialInterface(port='/dev/tty.usbserial-FT9I6BLV', baud=921600)
